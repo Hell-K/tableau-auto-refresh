83b8bf31d799f1d142d996e73473efe79b65538f
Kareem Abdelkader <kareemsherif22@gmail.com>
2024-06-14 13:25:54 +0200

    Initial test

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# tableau-auto-refresh
+# Refresh All Data Sources
+
+A simple example of a way to auto refresh all data sources present on a Tableau dashboard at regular time intervals.
+
+## How to use
+
+### Using directly from my server
+
+Download the `RefreshAll.trex` file from this repo and drag in the extension, select **My Extension** and select the downloaded .trex file.
+
+### Self Hosted
+
+Download the complete source repository, customize the JS and HTML if needed and host them in your web server. Update the url in the .trex file with the web server where extension is hosted.
+
+```xml
+...
+<url>https://yourcompany.com/autorefresh-datasources-tableau/</url>
+...
+
+```
+
+_Main source for refreshing the data source is forked from https://github.com/KeshiaRose/refresh-all-example_
---
+++ scripts.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", function () {
+	tableau.extensions.initializeDialogAsync().then(() => {
+		let timer;
+		try {
+			timer = setInterval(refreshAllDataSources, 5000);
+			console.log("Refresh Started");
+		} catch (err) {
+			clearInterval(timer);
+		}
+	});
+});
+
+function refreshAllDataSources() {
+	document.getElementById("refresh").innerHTML = "Refreshing Data Sources";
+	let dataSourceFetchPromises = [];
+	let dashboardDataSources = {};
+	const dashboard = tableau.extensions.dashboardContent.dashboard;
+
+	dashboard.worksheets.forEach(function (worksheet) {
+		dataSourceFetchPromises.push(worksheet.getDataSourcesAsync());
+	});
+
+	Promise.all(dataSourceFetchPromises)
+		.then(function (fetchResults) {
+			fetchResults.forEach(function (dataSourcesForWorksheet) {
+				dataSourcesForWorksheet.forEach(function (dataSource) {
+					if (!dashboardDataSources[dataSource.id]) {
+						dashboardDataSources[dataSource.id] = dataSource;
+						dataSource.refreshAsync();
+					}
+				});
+			});
+		})
+		.then(() => {
+			document.getElementById("refresh").innerHTML = "";
+		});
+}
